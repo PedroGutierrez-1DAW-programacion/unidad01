4320b02dd8509af634ddfd8d6f4fd2ff3526db14
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-10-17 20:21:04 +0200

    (ej 5) Añadido velocidad de descarga

---
+++ Unidad 1/src/pro2/ej03_velocidad.java
@@ -0,0 +1,22 @@
+package pro2;
+
+import java.util.Scanner;
+
+public class ej03_velocidad {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int MB, Mbps, time;
+		
+		System.out.println("Tamaño de archivo (MB):");
+		MB = teclado.nextInt() * 8; // Conversión Bit / Byte
+		
+		System.out.println("Velocidad de conexión (Mbps):");
+		Mbps = teclado.nextInt();
+		
+		time = MB / Mbps;
+		
+		System.out.println("Tiempo para la descarga: " + time + " segundos");
+		teclado.close();
+	}
+}
