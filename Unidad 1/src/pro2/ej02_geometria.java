e1491d441f0e3a48ac45d358e4f6b9539f8a825e
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-10-17 19:42:22 +0200

    (ej3) Geometría mejorado con programa selector

--- Unidad 1/src/pro2/ej02_geometria.java
+++ Unidad 1/src/pro2/ej02_geometria.java
@@ -8,16 +8,96 @@
 
 package pro2;
 
+import java.util.Scanner;
+
 public class ej02_geometria {
+	
+	static String[] funciones = new String[7];
 
 	public static void main(String[] args) {
-		System.out.println( "perimetoRectangulo:		" +	perimetoRectangulo(5, 10) );
-		System.out.println( "areaRectangulo:			" + areaRectangulo(5, 10) );
-		System.out.println( "perimetroCirculo:		" + perimetroCirculo(6) );
-		System.out.println( "areaCirculo:			" + areaCirculo(7) );
-		System.out.println( "volumenEsfera:			" + volumenEsfera(20) );
-		System.out.println( "areaRectanguloCoords:		" + areaRectanguloCoords(5, 5, 10, 15) );
-		System.out.println( "hipotenusaTriangulo:		" + hipotenusaTriangulo(3, 4) );
+		int s, s1, s2, s3, s4;
+		Scanner teclado = new Scanner(System.in);
+		
+		funciones[0] = "Perimetro de Rectángulo";
+		funciones[1] = "Área de Rectángulo";
+		funciones[2] = "Perímetro de Círculo";
+		funciones[3] = "Área de Círculo";
+		funciones[4] = "Volumen de esfera";
+		funciones[5] = "Área de Recángulo (coordenadas)";
+		funciones[6] = "Hipotenusa de Triángulo";
+		
+		System.out.println("Funciones:");
+		for(int i=0; i<funciones.length; i++)
+			System.out.println("[" + i + "] " + funciones[i]);
+		System.out.println("Número de función:");
+		s = teclado.nextInt();
+		System.out.println("-------------------------\n" + funciones[s]);
+		switch(s){
+			case 0:
+				System.out.println("Ancho:");
+				s1 = teclado.nextInt();
+				System.out.println("Alto:");
+				s2 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [" + s1 + ", " + s2 + "] = ");
+				System.out.print(perimetoRectangulo(s1, s2));
+				break;
+			case 1:
+				System.out.println("Ancho:");
+				s1 = teclado.nextInt();
+				System.out.println("Alto:");
+				s2 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [" + s1 + ", " + s2 + "] = ");
+				System.out.print(areaRectangulo(s1, s2));
+				break;
+			case 2:
+				System.out.println("Radio:");
+				s1 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [" + s1 + "] = ");
+				System.out.print(perimetroCirculo(s1));
+				break;
+			case 3:
+				System.out.println("Radio:");
+				s1 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [" + s1 + "] = ");
+				System.out.print(areaCirculo(s1));
+				break;
+			case 4:
+				System.out.println("Radio:");
+				s1 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [" + s1 + "] = ");
+				System.out.print(volumenEsfera(s1));
+				break;
+			case 5:
+				System.out.println("Coordenada X1:");
+				s1 = teclado.nextInt();
+				System.out.println("Coordenada Y1:");
+				s2 = teclado.nextInt();
+				System.out.println("Coordenada X2:");
+				s3 = teclado.nextInt();
+				System.out.println("Coordenada Y2:");
+				s4 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [(" + s1 + ", " + s2 + "), (" + s3 + ", " + s4 +")] = ");
+				System.out.print(areaRectanguloCoords(s1, s2, s3, s4));
+				break;
+			case 6:
+				System.out.println("Cateto 1:");
+				s1 = teclado.nextInt();
+				System.out.println("Cateto 2:");
+				s2 = teclado.nextInt();
+				System.out.print(funciones[s]);
+				System.out.print(" [" + s1 + ", " + s2 + "] = ");
+				System.out.print(hipotenusaTriangulo(s1, s2));
+				break;
+			default: System.out.println("[Error] Número no válido");
+		}
+		teclado.close();
+		
 	}
 	public static int perimetoRectangulo(int w, int h){
 		return (2 * w) + (2 * h);
@@ -41,7 +121,7 @@ public class ej02_geometria {
 		int w, h;
 		w = x2 - x1;
 		h = y2 - y1;
-		return (2 * w) + (2 * h);
+		return w * h;
 	}
 	public static double hipotenusaTriangulo(int c1, int c2){
 		// h^2 = c1^2 + c2^2 
