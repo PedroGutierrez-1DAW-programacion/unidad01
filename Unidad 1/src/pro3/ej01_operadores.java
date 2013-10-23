c8bc9d2ca28dc5cdf56798b4223cfb2ac90d9dd4
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-10-23 19:42:31 +0200

    Ampliado prueba de operadores

--- Unidad 1/src/pro3/ej01_operadores.java
+++ Unidad 1/src/pro3/ej01_operadores.java
@@ -5,7 +5,7 @@ public class ej01_operadores {
 		// (-5^2 + 10) * 3
 		System.out.println( (Math.pow(-5, 2) + 10) * 3 );
 		// 25.3 - 53 / 10
-		System.out.println( 25.3 - 53 / 10 );
+		System.out.println( 35.3 - 53 / 10 );
 		// 1 + 5 % 1.5 * 8 / 2
 		System.out.println( 1 + 5 % 1.5 * 8 / 2 );
 		// - (-2^5 + 18 / 5)
@@ -19,6 +19,18 @@ public class ej01_operadores {
 		// (|4 - 20 / 3 |)^3
 		System.out.println( Math.pow((4 - 20 / 3 ), 3) );
 		// sqrt(4.5 - 5.0) + 7 * 3
-		System.out.println( Math.sqrt(4.5 - 5.0) + 7 * 3 );	
+		System.out.println( Math.sqrt(4.5 - 5.0) + 7 * 3 );
+		// 3 * 30 / 3 + 10 % 3
+		System.out.println( 3 * 30 / 3 + 10 % 3 );
+		// 3L^3
+		System.out.println( Math.pow(3l, 3) );
+		// (36.0 / 4) + (5 * 3)
+		System.out.println( (36.0 / 4) + (5 * 3) );
+		// (28 / 7) * 2
+		System.out.println( (28 / 7) * 2 );
+		// (13 * 3) % 10
+		System.out.println( (13 * 3) % 10 );
+		// 2 ^ 3 ^ 3
+		System.out.println( Math.pow(2, Math.pow(3, 3) ) );
 	}
 }
