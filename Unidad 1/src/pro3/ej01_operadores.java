2ac8b43ab16299778e0fbe98710d78d41ebeecb6
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-10-25 17:27:28 +0200

    Añadido ejercicio con mil y un operadores

--- Unidad 1/src/pro3/ej01_operadores.java
+++ Unidad 1/src/pro3/ej01_operadores.java
@@ -11,13 +11,13 @@ public class ej01_operadores {
 		// - (-2^5 + 18 / 5)
 		System.out.println( - (Math.pow(-2, 5) + 18 / 5) );
 		// 65 / 3 / 4.0 * 3
-		System.out.println( 65 / 3 / 4.0 * 3 );
+		System.out.println(65 / 3 / 4.0 * 3 );
 		// 44 % 10 + 7 - 25^10 - 2
 		System.out.println( 44 % 10 + 7 - Math.pow(25, 10) - 2 );
 		// 10 % 4 + 6 / 2
 		System.out.println( 10 % 4 + 6 / 2 );
 		// (|4 - 20 / 3 |)^3
-		System.out.println( Math.pow((4 - 20 / 3 ), 3) );
+		System.out.println( Math.pow(Math.abs(4 - 20 / 3 ), 3) );
 		// sqrt(4.5 - 5.0) + 7 * 3
 		System.out.println( Math.sqrt(4.5 - 5.0) + 7 * 3 );
 		// 3 * 30 / 3 + 10 % 3
